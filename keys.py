16b052953f3ad3ed34cd6b7647b3dc8a87410da3
stevescilar <stevemuambi@gmail.com>
2022-05-05 16:53:35 +0300

    updated

---
+++ keys.py
@@ -0,0 +1,6 @@
+busines_short_code = "174379"
+phone_number = "254705549257"
+LNM_passkey ="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+consumer_key = "EGqJGcKGAoS95gctDXNJt1WwGjMfJdZl"
+consumer_secret = "RUhmLVy8QgLzFXtL"
+
--- lipanampesa.py
+++ lipanampesa.py
@@ -1,1 +1,35 @@
-print("it works very well")
+import base64
+from datetime import datetime
+import requests
+import keys
+
+
+
+unformatted_time = datetime.now()
+formatted_time = unformatted_time.strftime("%Y%m%d%H%M%S")
+
+data_to_encode = keys.busines_short_code + keys.LNM_passkey + formatted_time
+encoded = base64.b64encode(data_to_encode.encode())
+decoded_password = encoded.decode('utf-8')
+
+def lipa_na_mpesa():
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer LCXsWuHBoKjjsacHricBk39SxDh5'
+    }
+    payload = {
+        "BusinessShortCode": keys.busines_short_code,
+        "Password": decoded_password,
+        "Timestamp": formatted_time,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": keys.phone_number,
+        "PartyB": keys.busines_short_code,
+        "PhoneNumber": keys.phone_number,
+        "CallBackURL": "https://mydomain.com/path",
+        "AccountReference": "CompanyXLTD",
+        "TransactionDesc": "Payment of 1" 
+    }
+    response = requests.request("POST", 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest', headers = headers, data = payload)
+
+    print(response.text.encode('utf8'))
