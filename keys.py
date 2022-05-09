88c10cfbbebf5176c1e41d5d44fdb9a8bfd483e6
stevescilar <stevemuambi@gmail.com>
2022-05-09 11:53:29 +0300

    updated

--- keys.py
+++ keys.py
@@ -1,6 +1,6 @@
 business_short_code = "174379"
-partyA = "254705549257"
-phone_number = "254705549257"
+partyA = "254715186823"
+phone_number = "254715186823"
 LNM_passkey ="bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
 consumer_key = "EGqJGcKGAoS95gctDXNJt1WwGjMfJdZl"
 consumer_secret = "RUhmLVy8QgLzFXtL"
